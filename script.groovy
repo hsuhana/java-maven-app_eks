64954b52e8d7dfaa0ffc679ecd60ff57fdc4819f
Tracy <iamnotliaml@gmail.com>
2025-12-26 11:52:01 -0500

    add check

--- script.groovy
+++ script.groovy
@@ -14,6 +14,10 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
+    sh '''
+        aws sts get-caller-identity
+        kubectl get nodes
+    '''
     sh 'kubectl create deployment nginx-deployment --image=nginx'
 }
 
